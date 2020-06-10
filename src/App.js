f14ae30bb97bbd04d89aff9ff95268057d891c5a
sarika296 <sarikadewangan94@gmail.com>
2020-06-11 00:46:14 +0530

    redux store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './style.css';
+import { Provider } from 'react-redux'
+import store from './redux/store'
 import CakeContainer from './components/CakeContainer';
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer />
-    </div>
+    <Provider store = {store}>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
   );
 }
 
