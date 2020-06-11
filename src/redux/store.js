b7e7fd37acc75c099364d7a1f42f6a6449e3d62f
sarika296 <sarikadewangan94@gmail.com>
2020-06-11 17:35:24 +0530

    redux thunk get request

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from '../redux/rootReducer'
 import logger from 'redux-logger'
 
-const store= createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)) )
+const store= createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk)) )
 
 export default store 
